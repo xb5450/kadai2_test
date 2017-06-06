f182965bea74c85a2bfd847748085fc24c87d4f8
Atsushi SHIBATA <zhengjianping@zhengjianping-no-MacBook-Air.local>
2017-06-06 18:38:53 +0900

    test of kadai2

---
+++ gcd.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+read -p "二つの自然数を入手してくだい：" a b
+
+num1=$a
+num2=$b
+r=1
+until [ "$r" -eq 0 ]
+do
+  r=$(($num1%$num2))
+  num1=$num2
+  num2=$r
+done
+
+ echo " $a and $b の最大公約数 = $num1"
