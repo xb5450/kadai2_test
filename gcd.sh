ff81d8799b664d88c6ec8f9fed8e7fc33a571934
Atsushi SHIBATA <zhengjianping@zhengjianping-no-MacBook-Air.local>
2017-06-09 09:39:42 +0800

    change to new gcd_test.sh

--- gcd.sh
+++ gcd.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# arguments check
-if [ $# -lt 2 ]; then
-  echo "gcd.sh requires 2 inputs" 1 >&2
-  exit 1
-fi
-
-# main
 num1=$1
 num2=$2
 r=1
 until [ "$r" -eq 0 ]
 do
-  r=$(( $num1 % $num2 ))
+  r=$(($num1%$num2))
   num1=$num2
   num2=$r
 done
 
  echo "$1 $2 $num1"
+
--- gcd_test.sh
+++ gcd_test.sh
@@ -9,19 +9,14 @@ Error_Exit () {
 }
 
 echo "24 48 24" > $tmp-ans
-./gcd.sh 24 48> $tmp-out || Error_Exit "test1-1"
+./gcd.sh 24 48 > $tmp-out || Error_Exit "test1-1"
 echo "8 128 8" > $tmp-ans
-./gcd.sh 8 128> $tmp-out || Error_Exit "test2-1"
+./gcd.sh 8 128 > $tmp-out || Error_Exit "test2-1"
 echo "7 77 7" > $tmp-ans
-./gcd.sh 7 77> $tmp-out || Error_Exit "test3-1"
+./gcd.sh 7 77 > $tmp-out || Error_Exit "test3-1"
 echo "12 15 3" > $tmp-ans
-./gcd.sh 12 15> $tmp-out || Error_Exit "test4-1"
+./gcd.sh 12 15 > $tmp-out || Error_Exit "test4-1"
 diff $tmp-ans $tmp-out || Error_Exit "test-2"
-
-echo "gcd.sh requires 2 inputs" > $tmp-ans1
-./gcd.sh 22> $tmp-err && Error_Exit "test-err"
-diff $tmp-ans1 $tmp-err || Error_Exit "test-2" 
-
 
 echo OK
 rm -f $tmp-*
