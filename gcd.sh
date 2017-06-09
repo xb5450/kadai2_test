eea084c062603b9f0980127a60de4a9debe42279
Atsushi SHIBATA <zhengjianping@zhengjianping-no-MacBook-Air.local>
2017-06-09 10:06:28 +0800

    change gcd.sh and gcd_test.sh

--- gcd.sh
+++ gcd.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+  echo "gcd.sh requires 2 string inputs"
+  exit 1
+fi
+
 num1=$1
 num2=$2
 r=1
 until [ "$r" -eq 0 ]
 do
-  r=$(($num1%$num2))
+  r=$(($num1 % $num2))
   num1=$num2
   num2=$r
 done
--- gcd_test.sh
+++ gcd_test.sh
@@ -8,17 +8,22 @@ Error_Exit () {
     exit 1
 }
 
-echo "24 48 24" > $tmp-ans
+echo 24 48 24 > $tmp-ans
 ./gcd.sh 24 48 > $tmp-out || Error_Exit "test1-1"
-echo "8 128 8" > $tmp-ans
+echo 8 128 8 > $tmp-ans
 ./gcd.sh 8 128 > $tmp-out || Error_Exit "test2-1"
-echo "7 77 7" > $tmp-ans
+echo 7 77 7 > $tmp-ans
 ./gcd.sh 7 77 > $tmp-out || Error_Exit "test3-1"
-echo "12 15 3" > $tmp-ans
+echo 12 15 3 > $tmp-ans
 ./gcd.sh 12 15 > $tmp-out || Error_Exit "test4-1"
 diff $tmp-ans $tmp-out || Error_Exit "test-2"
 
+echo gcd.sh requires 2 string inputs > $tmp-ans
+./gcd.sh 22 > $tmp-err && Rrror_Exit "test-err"
+diff $tmp-ans $tmp-err || Error_Exit "test-2"
+
 echo OK
+echo $tmp-ans
 rm -f $tmp-*
 exit 0
 
