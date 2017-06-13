1bbf9f3d67206b3a88bded2b91231a6fb01b97fb
Atsushi SHIBATA <zhengjianping@zhengjianping-no-MacBook-Air.local>
2017-06-13 17:25:11 +0800

    change to new code

--- gcd_test.sh
+++ gcd_test.sh
@@ -3,7 +3,7 @@
 tmp=/tmp/$$
 
 Error_Exit () {
-    echo "$1" >&2
+    echo $1 1>&2
     rm -f $tmp-*
     exit 1
 }
@@ -13,20 +13,21 @@ echo 24 78 6 > $tmp-ans
 diff $tmp-ans $tmp-out || Error_Exit "TEST1-2"
 
 echo 7 77 7 > $tmp-ans
-./gcd_err.sh 7 77 > $tmp-out || Error_Exit "TEST1-1"
-diff $tmp-ans $tmp-out || Error_Exit "TEST1-2"
+./gcd_err.sh 7 77 > $tmp-out || Error_Exit "TEST2-1"
+diff $tmp-ans $tmp-out || Error_Exit "TEST2-2"
 
 echo 3 8 1 > $tmp-ans
-./gcd_err.sh 3 8 > $tmp-out || Error_Exit "TEST1-1"
-diff $tmp-ans $tmp-out || Error_Exit "TEST1-2"
+./gcd_err.sh 3 8 > $tmp-out || Error_Exit "TEST3-1"
+diff $tmp-ans $tmp-out || Error_Exit "TEST3-2"
 
 echo 15 35 5 > $tmp-ans
-./gcd_err.sh 15 35 > $tmp-out || Error_Exit "TEST1-1"
-diff $tmp-ans $tmp-out || Error_Exit "TEST1-2"
+./gcd_err.sh 15 35 > $tmp-out || Error_Exit "TEST4-1"
+diff $tmp-ans $tmp-out || Error_Exit "TEST4-2"
 
-#echo "gcd_err.sh need two inputs" > $tmp-ans
-#./gcd_err.sh 2 > $tmp-out && Error_Exit "TEST2-1"
-#diff $tmp-ans $tmp-out || Error_Exit "TEST2-2"
+#test err
+echo "gcd_err.sh need two inputs" > $tmp-ans
+./gcd_err.sh  2> $tmp-err && Error_Exit "TEST_err-1"
+diff $tmp-ans $tmp-err || Error_Exit "TEST_err-2"
 
 echo OK
 rm -f $tmp-*
